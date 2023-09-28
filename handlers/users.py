962fddb27b00d8f268862279d773dc4e49aa9c27
Fugguri <fygguri@icloud.com>
2023-09-28 20:33:40 +0300

    update

--- handlers/users.py
+++ handlers/users.py
@@ -109,7 +109,8 @@ async def start_mail(callback: types.CallbackQuery, state: FSMContext, callback_
         return
     groups = selected_groups.values()
     recp = await user_bot.get_members_from_chats(groups)
-
+    print(recp)
+    return
     await callback.message.delete()
     if callback_data['all'] == "False":
         numbers = []
